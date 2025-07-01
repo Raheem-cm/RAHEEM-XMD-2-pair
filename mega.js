4a5f7e036b7fa3ebf27fe4a2649d2c9068381c0c
Raheem-cm <nyoniraheem@gmail.com>
2025-07-02 01:41:39 +0300

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 const mega = require("megajs");
 
 const auth = {
-    email: 'sbot7603@gmail.com',   //use your real vaild mega account email
-    password: 'sumon@983655',  ////use your real vaild mega account password
+    email: 'chiefmamba700@gmail.com',   //use your real vaild mega account email
+    password: 'Raheem@2557',  ////use your real vaild mega account password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
